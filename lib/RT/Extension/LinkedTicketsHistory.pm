b74d12ff004210ce24a563e2ec915f3c1c9b930f
sunnavy <sunnavy@bestpractical.com>
2015-11-05 05:40:04 +0800

    document the new Plugin syntax for 4.2+

--- lib/RT/Extension/LinkedTicketsHistory.pm
+++ lib/RT/Extension/LinkedTicketsHistory.pm
@@ -22,7 +22,11 @@ May need root permissions
 
 =item Edit your F</opt/rt4/etc/RT_SiteConfig.pm>
 
-Add this line:
+If you are using RT 4.2 or greater, add this line:
+
+    Plugin('RT::Extension::LinkedTicketsHistory');
+
+For RT 4.0, add this line:
 
     Set(@Plugins, qw(RT::Extension::LinkedTicketsHistory));
 
